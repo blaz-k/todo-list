4e32215f352f17c463d188698a560b0e000524f2
Blaz <blazyy@gmail.com>
2022-01-04 12:20:23 +0100

    navbar updated; fontawesome added; minor changes in about

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,27 +1,42 @@
 <template>
   <nav class="navbar sticky-top navbar-expand-lg navbar-light bg-light">
     <div class="container-fluid">
-      <router-link to="/" class="navbar-brand">Navbar</router-link>
+      <router-link to="/" class="navbar-brand">Web3 Todo List</router-link>
 
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
         <span class="navbar-toggler-icon"></span>
       </button>
 
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-
           <li class="nav-item">
-            <router-link class="nav-link active" aria-current="page" to="/">Home</router-link>
+            <router-link
+              class="nav-link active"
+              aria-current="page"
+              :to="{ name: 'Home' }"
+              >Home</router-link
+            >
           </li>
 
           <li class="nav-item">
-            <router-link class="nav-link" to="/about">About</router-link>
+            <router-link class="nav-link" :to="{ name: 'About' }"
+              >About</router-link
+            >
           </li>
 
           <li class="nav-item">
-            <router-link class="nav-link" to="/profile">Profile</router-link>
+            <router-link class="nav-link" :to="{ name: 'Profile' }"
+              >Profile</router-link
+            >
           </li>
-
         </ul>
 
         <div class="d-flex">
@@ -36,27 +51,33 @@
             </button>
           </div>
 
-          <button v-else class="btn btn-outline-success" @click="open">Connect wallet</button>
+          <button v-else class="btn btn-outline-success" @click="open">
+            Connect wallet
+          </button>
         </div>
-
       </div>
     </div>
   </nav>
 </template>
 
 <script lang="ts">
-import { useBoard, useEthers, displayEther, shortenAddress } from 'vue-dapp'
+import { useBoard, useEthers, displayEther, shortenAddress } from "vue-dapp";
 
 export default {
   name: "Navbar",
-  
+
   setup() {
-    const { open } = useBoard()
-    const { address, balance, isActivated } = useEthers()
+    const { open } = useBoard();
+    const { address, balance, isActivated } = useEthers();
 
     return {
-      address, balance, isActivated, open, displayEther, shortenAddress
-    }
-  }
-}
+      address,
+      balance,
+      isActivated,
+      open,
+      displayEther,
+      shortenAddress,
+    };
+  },
+};
 </script>
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,9 +1,14 @@
 <template>
   <div class="text-center">About</div>
+  <p>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima mollitia
+    officia reiciendis animi eaque quis adipisci! Natus id dolore corrupti
+    eveniet culpa voluptas ad! Animi.
+  </p>
 </template>
 
 <script lang="ts">
-  export default {
-    name: "About"
-  }
+export default {
+  name: "About",
+};
 </script>
