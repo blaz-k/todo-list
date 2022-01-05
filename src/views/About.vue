ed578c059303c76d2ac71bcd41bb6ad3a5cc82eb
Blaz <blazyy@gmail.com>
2022-01-05 09:16:41 +0100

    mior style changes in about

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,10 +1,13 @@
 <template>
-  <div class="text-center">About</div>
-  <p>
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima mollitia
-    officia reiciendis animi eaque quis adipisci! Natus id dolore corrupti
-    eveniet culpa voluptas ad! Animi.
-  </p>
+  <h2 class="text-center">About</h2>
+  <div class="about">
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima mollitia
+      officia reiciendis animi eaque quis adipisci! Natus id dolore corrupti
+      eveniet culpa voluptas ad! Animi. Lorem ipsum, dolor sit amet consectetur
+      adipisicing elit. Explicabo beatae molestiae quaerat hic itaque facere?
+    </p>
+  </div>
 </template>
 
 <script lang="ts">
@@ -12,3 +15,9 @@ export default {
   name: "About",
 };
 </script>
+<style scoped>
+.about {
+  display: flex;
+  text-align: center;
+}
+</style>
